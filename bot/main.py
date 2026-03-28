e8f248ba1903ed5206963435ca9a737511e12db5
Monem08 <monsiakhatun@gmail.com>
2026-03-28 21:24:15 +0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,27 @@
 from aiogram import executor
 from bot.loader import dp
-
 import bot.handlers.start
 import bot.handlers.menu
 import bot.handlers.scanner
 
-def start_bot():
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive 😈"
+
+
+def run_bot():
     executor.start_polling(dp, skip_updates=True)
+
+
+def start_bot():
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=10000)
+
 
 if __name__ == "__main__":
     start_bot()
