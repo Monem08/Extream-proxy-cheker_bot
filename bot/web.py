952aab07f19b5d97ca93a2ec2ecb04242a8c1b59
Monem08 <monsiakhatun@gmail.com>
2026-03-29 16:41:27 +0600

    Update web.py

--- bot/web.py
+++ bot/web.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "OK"
+    return Response("OK", status=200, mimetype="text/plain")
